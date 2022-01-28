88d5d2fb9ebe09aa2601eddd44fe5cab4f0d316c
Gregorio Reyes <grreyesc@hotmail.com>
2022-01-28 00:14:06 -0600

    Initial commit

---
+++ src/test/java/com/example/SoloJava/SoloJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SoloJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoloJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
